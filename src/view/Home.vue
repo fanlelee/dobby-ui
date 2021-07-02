f78d7c0288b023e33dc1bd56c16290b77ed034ab
fanlelee <529378687@qq.com>
2021-07-02 15:52:32 +0800

    添加Home的样式

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -1,18 +1,43 @@
 <template>
   <topnav></topnav>
   <div class="banner">
-    <h1>Dobby UI</h1>
-    <p>Dobby(多比)是我家可爱猫咪的名字</p>
-    <button>开始</button>
+    <h2 class="tittle">Dobby UI</h2>
+    <p class="detail">Dobby(多比)是我家可爱猫咪的名字</p>
+    <div class="lead">
+      <a>GitHub</a>
+      <a>文档</a>
+    </div>
   </div>
 </template>
 <script lang="ts">
 import Topnav from "../components/Topnav.vue";
+
 export default {
-  name:'Home',
-  components:{
+  name: 'Home',
+  components: {
     Topnav
   }
 }
 </script>
+<style lang="scss" scoped>
+  .banner{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: lightsteelblue;
+    padding: 80px 0;
+    >.lead{
+      a{
+        background-color: #fff;
+        $h:30px;
+        border-radius: $h/2;
+        padding: 0 10px;
+        margin: 3px;
+        height: $h;
+        line-height: $h;
+        display: inline-block;
+      }
+    }
+  }
+</style>
 
