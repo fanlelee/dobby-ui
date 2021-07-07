613edb1bdcbc51a67b21da0bc703ba6ab152dcb0
fanlelee <529378687@qq.com>
2021-07-07 16:34:07 +0800

    创建组件Button，实现绑定事件

--- src/components/ButtonDoc.vue
+++ src/components/ButtonDoc.vue
@@ -1,9 +1,18 @@
 <template>
-  <div>i'm button</div>
+  <Button size="small" @click="x">it's button</Button>
 </template>
 <script>
+import Button from '../lib/Button.vue'
+
 export default {
-  name: "ButtonDoc"
+  name: "ButtonDoc",
+  components: {Button},
+  setup() {
+    const x = () => {
+      console.log('hi');
+    }
+    return {x}
+  }
 }
 </script>
 
---
+++ src/lib/Button.vue
@@ -0,0 +1,22 @@
+<template>
+  <div >
+    <button v-bind="rest">
+      <slot></slot>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  inheritAttrs:false,
+  setup(props, context){
+    const {size, ...rest} = context.attrs
+    console.log(size);
+    return {size, rest}
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
